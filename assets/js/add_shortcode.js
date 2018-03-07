a7b326b4d5e193e47222e113a181679151dfe4f1
Cristina <cristina.soproncu@gmail.com>
2018-03-07 07:24:55 +0200

    Aded dropdown input field

--- assets/js/add_shortcode.js
+++ assets/js/add_shortcode.js
@@ -134,6 +134,9 @@ editor = '';
 				var fill_details = function( $content ) {
 					clean_details();
 					details.html( $content ).addClass( 'active' );
+
+                    //reInitialize materialize select
+                    $(details).find('.input-field select:not(".initialized")').material_select();
 				};
 
 				//Change modal title
@@ -187,6 +190,7 @@ editor = '';
 						} );
 						editor = ed;
 						get_current_editor_selected_content = function() {
+
 							return editor;
 						};
 
@@ -301,10 +305,6 @@ editor = '';
 		return return_els;
 	};
 
-    setTimeout(function(){
-        $('select').material_select();
-    },1000);
-
 	/*
 	 * jQuery Reveal Plugin 1.0
 	 * www.ZURB.com
@@ -314,5 +314,4 @@ editor = '';
 	 */
 	$.fn.reveal=function(e){var t={animation:"fadeAndPop",animationspeed:300,closeonbackgroundclick:true,dismissmodalclass:"close-reveal-modal"};var e=$.extend({},t,e);return this.each(function(){function u(){i=false}function a(){i=true}var t=$(this),n=parseInt(t.css("top")),r=t.height()+n,i=false,s=$(".reveal-modal-bg");if(s.length==0){s=$('<div class="reveal-modal-bg" />').insertAfter(t)}t.bind("reveal:open",function(){s.unbind("click.modalEvent");$("."+e.dismissmodalclass).unbind("click.modalEvent");if(!i){a();if(e.animation=="fadeAndPop"){t.css({top:$(document).scrollTop()-r,opacity:0,visibility:"visible"});s.fadeIn(e.animationspeed/2);t.delay(e.animationspeed/2).animate({top:$(document).scrollTop()+n+"px",opacity:1},e.animationspeed,u())}if(e.animation=="fade"){t.css({opacity:0,visibility:"visible",top:$(document).scrollTop()+n});s.fadeIn(e.animationspeed/2);t.delay(e.animationspeed/2).animate({opacity:1},e.animationspeed,u())}if(e.animation=="none"){t.css({visibility:"visible",top:$(document).scrollTop()+n});s.css({display:"block"});u()}}t.unbind("reveal:open")});t.bind("reveal:close",function(){if(!i){a();if(e.animation=="fadeAndPop"){s.delay(e.animationspeed).fadeOut(e.animationspeed);t.animate({top:$(document).scrollTop()-r+"px",opacity:0},e.animationspeed/2,function(){t.css({top:n,opacity:1,visibility:"hidden"});u()})}if(e.animation=="fade"){s.delay(e.animationspeed).fadeOut(e.animationspeed);t.animate({opacity:0},e.animationspeed,function(){t.css({opacity:1,visibility:"hidden",top:n});u()})}if(e.animation=="none"){t.css({visibility:"hidden",top:n});s.css({display:"none"})}}t.unbind("reveal:close")});t.trigger("reveal:open");var o=$("."+e.dismissmodalclass).bind("click.modalEvent",function(){t.trigger("reveal:close")});if(e.closeonbackgroundclick){s.css({cursor:"pointer"});s.bind("click.modalEvent",function(){t.trigger("reveal:close")})}$("body").keyup(function(e){if(e.which===27){t.trigger("reveal:close")}})})}
 
-	console.log('add loaded');
 })(jQuery);

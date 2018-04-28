0da8435bc8170993bc663ad1e705d9f6bf77f7eb
Alexander Sava <fabian.sava@yahoo.com>
2018-04-28 23:13:54 +0300

    parallax

--- shortcodes/templates/parallax.php
+++ shortcodes/templates/parallax.php
@@ -2,5 +2,8 @@
 
 ?>
 <div class="parallax-container">
-    <div class="parallax"><img src="<?php echo $image; ?>" alt="<?php echo !empty($name) ? $name : ''; ?>"></div>
+    <div class="parallax">
+        <h2 class="subheading"><?php echo $subtitle?></h2>
+        <h1 class="heading"><?php echo $title?></h1>
+        <img src="<?php echo $image; ?>" alt="<?php echo !empty($name) ? $name : ''; ?>"></div>
 </div>
